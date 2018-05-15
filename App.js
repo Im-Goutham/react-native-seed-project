57448d281afee8796891c5ec4064bb921496cef8
Goutham <gouthamrs4u@gmail.com>
2018-05-15 17:41:31 +0530

    Splash screen added for android

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import MainNavigator from './navigation/MainNavigator';
 import { Provider } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen'
 import { Root } from "native-base";
 
 import store from './store';
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+      	// do stuff while splash screen is shown
+          // After having done stuff (such as async tasks) hide the splash screen
+          SplashScreen.hide();
+  }
 
   render() {
       return (
--- android/app/src/main/java/com/navigation_sample/MainActivity.java
+++ android/app/src/main/java/com/navigation_sample/MainActivity.java
@@ -1,13 +1,17 @@
 package com.navigation_sample;
-
+import android.os.Bundle; //Include this//
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;//Include this//
+
 
 public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+  //Include this start//
+  @Override
+   protected void onCreate(Bundle savedInstanceState) {
+       SplashScreen.show(this, true);
+       super.onCreate(savedInstanceState);
+   }
+   //Include this end//
     @Override
     protected String getMainComponentName() {
         return "navigation_sample";
